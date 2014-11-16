867c9c66e97c29df75a272dafd0eb50e37aac27a
tonydisera <tony.disera@gmail.com>
2014-11-16 10:20:59 -0700

    change server names to production.

--- app/vcf.iobio.js
+++ app/vcf.iobio.js
@@ -63,10 +63,13 @@ vcfiobio = function module() {
   var SOURCE_TYPE_FILE = "file";
   var sourceType = "url";
 
-  var vcfstatsAliveServer    = "ws://localhost:7070";
-  var tabixServer            = "ws://localhost:7090";
-  var vcfReadDeptherServer   = "ws://localhost:7062";
+  //var vcfstatsAliveServer    = "ws://localhost:7070";
+  //var tabixServer            = "ws://localhost:7090";
+  //var vcfReadDeptherServer   = "ws://localhost:7062";
 
+  var vcfstatsAliveServer    = "ws://vcfstatsalive.iobio.io";
+  var tabixServer            = "ws://tabix.iobio.io";
+  var vcfReadDeptherServer   = "ws://vcfreaddepther.iobio.io";
 
 
   var vcfURL;
