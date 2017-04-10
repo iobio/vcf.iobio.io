iobio/vcf.iobio.io

commit 5f135d258dc68ccf20282b4fdbcd82eb58c41a13

Add selection of genome build so that correct reference lengths are shown for a vcf file.

app/app.js
@@ -84,6 +84,12 @@ var lookupNucleotide = {
 
 var colorListVarType = ["#2171b5", "#eff3ff", "#bdd7e7", "#6baed6", ];
 
+//var iobioServer           = "nv-green.iobio.io";
+var iobioServer           = "nv-dev.iobio.io/";
+var dataSelect            = null;
+var genomeBuildHelper     = null;
+var genomeBuildServer     = "http://" + iobioServer + "genomebuild/"; // !pointed to nv-dev
+
 
 /*
 *  Document initialization
@@ -91,8 +97,11 @@ var colorListVarType = ["#2171b5", "#eff3ff", "#bdd7e7", "#6baed6", ];
 $(document).ready( function(){
 	//d3BrowserAdjustments();
 
-	init();
-
+	genomeBuildHelper = new GenomeBuildHelper();
+	genomeBuildHelper.promiseInit().then(function() {
+		$('#build-label').text(genomeBuildHelper.getCurrentBuildName());
+		init();
+	});
 });
 
 /*
@@ -110,19 +119,19 @@ function init() {
 	vcfiobio = new vcfiobio();
 	vcfiobio.setSamples([]);
 
+	$('#url-input').focusout(function() {
+		vcfiobio.vcfURL = $('#url-input').val();
+	    dataSelect.setDefaultBuildFromData();
+  	});
+
 
 	// Setup event handlers for File input
 	document.getElementById('file').addEventListener('change', onFilesSelected, false);
 
-	$('#vcf-sample-select').selectize(
-		{
-			create: true,
-			maxItems: null,
-			valueField: 'value',
-	    	labelField: 'value',
-	    	searchField: ['value']
-		}
-	);
+	// Initialize the data selection widget
+	dataSelect = new DataSelect();
+	dataSelect.init();
+
 
 	// Get the container dimensions to determine the chart dimensions
 	getChartDimensions();
@@ -461,21 +470,62 @@ function displayVcfUrlBox() {
     $('#vcf-url').css('visibility','visible');
     $('#vcf-url-button-panel').css('visibility','visible');
     $("#vcf-url").children("input").focus();
+    $("#go-panel").removeClass("hide");
+    $("#url-go-button").removeClass("hide");
+    $("#file-go-button").addClass("hide");
+
 
 }
 function onFileButtonClicked() {
 	$('#vcf-url').css('visibility', 'hidden');
 	$('#vcf-url-button-panel').css('visibility', 'hidden');
+	$("#go-panel").removeClass("hide");
+    $("#url-go-button").addClass("hide");
+    $("#file-go-button").removeClass("hide");
+
 }
 
-function onUrlEntered() {
+function loadFromUrl() {
     var url = $("#url-input").val();
-    window.history.pushState({'index.html' : 'bar'},null,"?vcf=" + encodeURIComponent(url));
+    updateUrl("vcf",  encodeURIComponent(url));
     _loadVcfFromUrl(url);
 }
 
+function loadFromFile() {
+	$('.vcf-sample.loader').removeClass("hide");
+
+	d3.select("#selectData")
+	  .style("visibility", "hidden")
+	  .style("display", "none");
+
+	d3.select("#showData")
+	  .style("visibility", "visible");
+
+	vcfiobio.loadIndex(onReferencesLoading, onReferencesLoaded, displayFileError);
+
+}
+function updateUrl(paramName, value) {
+	var params = {};
+	// turn params into hash
+	window.location.search.split('&').forEach(function(param){
+		if (param != '') {
+			param = param.split('?').length == 1 ? param : param.split('?')[1];
+			var fields = param.split('=');
+			params[fields[0]] = fields[1];
+		}
+	});
+	params[paramName] = value;
+	var search = [];
+	Object.keys(params).forEach(function(key) {
+		search.push(key + '=' + params[key]);
+	})
+    window.history.replaceState(null,null,'?'+search.join('&'));	
+}
+
 function _loadVcfFromUrl(url, sampleNames) {
-	$("#file-alert").css("visibility", "hidden");
+	$("#file-alert").addClass("hide");
+	dataSelect.setDefaultBuildFromData();
+
 	if (sampleNames != null) {
 		vcfiobio.setSamples(sampleNames);
 	}
@@ -504,6 +554,10 @@ function _loadVcfFromUrl(url, sampleNames) {
 			displayFileError(message);
 
 			$('#vcf-url').css("visibility", "visible");
+			$("#go-panel").removeClass("hide");
+			$("url-go-button").removeClass("hide");
+			$("file-go-button").addClass("hide");
+
 			$("#url-input").focus();
 			$("#url-input").val(url);
 
@@ -514,22 +568,14 @@ function _loadVcfFromUrl(url, sampleNames) {
 
 }
 
+
+
 function onFilesSelected(event) {
-	$("#file-alert").css("visibility", "hidden");
+	$("#file-alert").addClass("hide");
 	vcfiobio.openVcfFile( event,
 		function(vcfFile) {
-			$('.vcf-sample.loader').removeClass("hide");
-
 			d3.select("#vcf_file").text(vcfFile.name);
-
-			d3.select("#selectData")
-			  .style("visibility", "hidden")
-			  .style("display", "none");
-
-			d3.select("#showData")
-			  .style("visibility", "visible");
-
-			vcfiobio.loadIndex(onReferencesLoading, onReferencesLoaded, displayFileError);
+			dataSelect.setDefaultBuildFromData();
 		},
 		function(errorMessage) {
 			displayFileError(errorMessage)
@@ -545,7 +591,7 @@ function displayFileError(errorMessage) {
 	  .style("visibility", "hidden");
 
 	$("#file-alert").text(errorMessage);
-	$("#file-alert").css("visibility", "visible");
+	$("#file-alert").removeClass("hide");
 }
 
 function showSamplesDialog() {

app/dataSelect.js
@@ -1,0 +1,174 @@
+function DataSelect() {
+	this.buildDefaulted = false;
+
+}
+
+DataSelect.prototype.init = function() {
+	var me = this;
+
+
+	$('#select-species').selectize(
+		{	
+			create: true, 			
+			valueField: 'name',
+	    	labelField: 'name',
+	    	searchField: ['name'],
+	    	maxItems: 1,
+	    	options: genomeBuildHelper.speciesList
+    	}
+	);
+	$('#select-build').selectize(
+		{
+			create: true, 			
+			valueField: 'name',
+	    	labelField: 'name',
+	    	searchField: ['name'],
+	    	onOptionAdd: function(value) {
+	    		if (!me.buildDefaulted) {
+	    			$('#select-build')[0].selectize.addItem(genomeBuildHelper.currentBuild.name, false);
+	    			me.buildDefaulted = true; 
+	    			updateUrl("build", genomeBuildHelper.currentBuild.name);
+	    		}
+	    	}
+    	}
+	);
+	me.addSpeciesListener();
+	$('#select-species')[0].selectize.addItem(genomeBuildHelper.getCurrentSpeciesName());
+	me.addBuildListener();
+	me.setDefaultBuildFromData();
+}
+
+DataSelect.prototype.removeSpeciesListener = function() {
+	if ($('#select-species')[0].selectize) {
+		$('#select-species')[0].selectize.off('change');
+	}
+}
+
+DataSelect.prototype.addSpeciesListener = function() {
+	var me = this;
+	if ($('#select-species')[0].selectize) {
+		$('#select-species')[0].selectize.on('change', function(value) {
+	        if (!value.length) {
+	        	return;
+	        }
+	        genomeBuildHelper.setCurrentSpecies(value);
+	        updateUrl("species", value);
+	        var selectizeBuild = $('#select-build')[0].selectize;
+	        selectizeBuild.disable();
+	        selectizeBuild.clearOptions();
+	        
+	        selectizeBuild.load(function(callback) {
+	        	selectizeBuild.enable();
+	        	callback(genomeBuildHelper.speciesToBuilds[value]);
+	        });
+			
+	    });
+	}
+
+}
+
+
+DataSelect.prototype.removeBuildListener = function() {
+	if ($('#select-build')[0].selectize) {
+		$('#select-build')[0].selectize.off('change');
+	}
+
+}
+
+DataSelect.prototype.addBuildListener = function() {
+	var me = this;
+	if ($('#select-build')[0].selectize) {
+	    $('#select-build')[0].selectize.on('change', function(value) {
+			if (!value.length) {
+				return;
+			}
+			genomeBuildHelper.setCurrentBuild(value);
+			updateUrl("build", value);
+			$('#build-link').text(value);
+			me.validateBuildFromData(function(success, message) {
+				if (success) {
+					$('#species-build-warning').addClass("hide");
+					//window.enableLoadButton();
+				} else {
+					$('#species-build-warning').html(message);
+					$('#species-build-warning').removeClass("hide");
+					//window.disableLoadButton();
+				}
+			});
+			
+		});
+	}
+
+}
+
+
+DataSelect.prototype.setDefaultBuildFromData = function() {
+	var me = this;
+	if ($('#select-species')[0].selectize && $('#select-build')[0].selectize) {
+		me.getBuildsFromData(function(buildsInData) {
+			if (buildsInData.length == 0) {
+				$('#species-build-warning').addClass("hide");
+				//window.enableLoadButton();
+
+			} else if (buildsInData.length == 1) {			
+				var buildInfo = buildsInData[0];
+				
+				me.removeBuildListener();
+				genomeBuildHelper.setCurrentSpecies(buildInfo.species.name);
+				genomeBuildHelper.setCurrentBuild(buildInfo.build.name);
+				$('#select-species')[0].selectize.setValue(buildInfo.species.name);
+				$('#select-build')[0].selectize.setValue(buildInfo.build.name);	
+				me.addBuildListener();
+
+				$('#species-build-warning').addClass("hide");				
+				//window.enableLoadButton();		
+			} else {
+				var message = genomeBuildHelper.formatIncompatibleBuildsMessage(buildsInData);
+				$('#species-build-warning').html(message);
+				$('#species-build-warning').removeClass("hide");
+				//window.disableLoadButton();
+			}
+		});		
+	}
+
+}
+
+DataSelect.prototype.validateBuildFromData = function(callback) {
+	var me = this;
+	me.getBuildsFromData(function(buildsInData) {
+		if (buildsInData.length == 0) {
+			callback(true);
+
+		} else if (buildsInData.length == 1) {
+			var buildInfo = buildsInData[0];
+			if (genomeBuildHelper.currentSpecies.name == buildInfo.species.name && genomeBuildHelper.currentBuild.name == buildInfo.build.name) {
+				callback(true);
+			} else {
+				callback(false, 'Incompatible build. Data files specify the genome build ' + buildInfo.species.name + ' ' + buildInfo.build.name);
+			}
+		} else {
+			callback(false, genomeBuildHelper.formatIncompatibleBuildsMessage(buildsInData));
+		}
+	});
+}
+
+
+DataSelect.prototype.getBuildsFromData = function(callback) {
+	var me = this;
+
+	me.getHeadersFromVcfs(function(vcfHeaderMap) {
+		var buildsInHeaders = genomeBuildHelper.getBuildsInHeaders({}, vcfHeaderMap);
+		callback(buildsInHeaders);
+
+	});
+}
+
+
+
+DataSelect.prototype.getHeadersFromVcfs = function(callback) {
+	var headerMap = {};
+	vcfiobio.getHeader(function(header) {
+		headerMap['proband'] = header;
+		callback(headerMap);
+	});
+}

app/genomeBuildHelper.js
@@ -1,0 +1,428 @@
+function GenomeBuildHelper() {
+	this.currentSpecies = null;
+	this.currentBuild = null;
+	this.speciesList = [];
+	this.speciesNameToSpecies = {}; // map species name to the species object
+	this.speciesToBuilds = {};      // map species to its genome builds
+	this.buildNameToBuild = {};     // 
+
+	this.DEFAULT_SPECIES = "Human";
+	this.DEFAULT_BUILD   = "GRCh37";
+
+	this.ALIAS_UCSC                   = "UCSC";
+
+	this.RESOURCE_CLINVAR_VCF_S3      = "CLINVAR VCF S3";
+	this.RESOURCE_CLINVAR_VCF_OFFLINE = "CLINVAR VCF OFFLINE";
+	this.RESOURCE_CLINVAR_POSITION    = "CLINVAR EUTILS BASE POSITION";
+	this.RESOURCE_ENSEMBL_URL         = "ENSEMBL URL";
+
+}
+
+GenomeBuildHelper.prototype.promiseInit = function() {
+	var me = this;
+	return new Promise(function(resolve, reject) {
+
+		$.ajax({
+	        url: genomeBuildServer,
+	        jsonp: "callback",
+			type: "GET",
+			dataType: "jsonp",
+	        error: function( xhr, status, errorThrown ) {
+			        
+			        console.log( "Error: " + errorThrown );
+			        console.log( "Status: " + status );
+			        console.log( xhr );
+			        reject("An error occurred when loading genomebuild data: " + errorThrown);
+			},
+	        success: function(allSpecies) {
+	        	
+	        	me.init(allSpecies);
+
+	        	resolve();
+	        }
+	    });
+
+	});
+
+}
+
+GenomeBuildHelper.prototype.init = function(allSpecies) {
+	var me = this;
+	allSpecies.forEach(function(species) {
+		// Map the species latin name to its species object
+		me.speciesNameToSpecies[species.name] = species;
+
+		// Collect all species into a list to use for dropdown
+		me.speciesList.push({name: species.name, value: species.name});
+
+		species.genomeBuilds.forEach(function(genomeBuild) {
+
+			// Map the build name to its build object
+			me.buildNameToBuild[genomeBuild.name] = genomeBuild;
+
+			// Map the species to its genome builds
+			var builds = me.speciesToBuilds[species.name];
+			if (builds == null) {
+				builds = [];
+				me.speciesToBuilds[species.name] = builds;
+			}
+			builds.push(genomeBuild);
+		
+		})
+	});
+
+	// Default the species and build
+	if (me.currentSpecies == null) {
+		me.currentSpecies = me.speciesNameToSpecies[me.DEFAULT_SPECIES];
+	}
+	if (me.currentBuild == null) {
+		me.currentBuild = me.buildNameToBuild[me.DEFAULT_BUILD];
+	}	
+}
+
+GenomeBuildHelper.prototype.setCurrentSpecies = function(speciesName) {
+	this.currentSpecies = this.speciesNameToSpecies[speciesName];
+}
+
+GenomeBuildHelper.prototype.setCurrentBuild = function(buildName) {
+	this.currentBuild = this.buildNameToBuild[buildName];
+}
+
+
+GenomeBuildHelper.prototype.getCurrentSpeciesName = function() {
+	return this.currentSpecies ? this.currentSpecies.name : null;
+}
+
+GenomeBuildHelper.prototype.getCurrentSpeciesLatinName = function() {
+	return this.currentSpecies ? this.currentSpecies.latin_name : null;
+}
+
+GenomeBuildHelper.prototype.getCurrentBuildName = function() {
+	return this.currentBuild ? this.currentBuild.name : null;
+}
+
+GenomeBuildHelper.prototype.getFastaPath = function(ref) {
+	var fastaPath = null;
+	if (this.currentBuild) {
+		this.currentBuild.references.forEach(function(theReference) {
+			if (!fastaPath) {
+				if (theReference.name == ref || theReference.alias == ref) {
+					if (ref.indexOf('chr') == 0) {
+						fastaPath = theReference.fastaPathUCSC;
+					} else {
+						fastaPath = theReference.fastaPathEnsembl;
+					}
+				}
+			}
+		});
+	}
+	return fastaPath;
+}
+
+GenomeBuildHelper.prototype.getReferenceLength = function(ref) {
+	var theRef =  this.getReference(ref);
+	return theRef ? theRef.length : null;
+}
+
+GenomeBuildHelper.prototype.getReference = function(ref) {
+	var theRef = null;
+	if (this.currentBuild) {
+		this.currentBuild.references.forEach(function(reference) {
+			if (!theRef) {
+				if (reference.name == ref || reference.alias == ref) {
+					theRef = reference;
+				}
+			}
+		});
+	}
+	return theRef;
+}
+
+GenomeBuildHelper.prototype.getBuildAlias = function(aliasType) {
+	var theAlias = null;
+	if (this.currentBuild) {
+		this.currentBuild.aliases.forEach(function(gbAlias) {
+			if (!theAlias && gbAlias.type == aliasType) {
+				theAlias = gbAlias.alias;
+			}
+		});
+	}
+	return theAlias;
+}
+
+GenomeBuildHelper.prototype.getBuildResource = function(resourceType) {
+	var theResource = null;
+	if (this.currentBuild) {
+		this.currentBuild.resources.forEach(function(gbResource) {
+			if (!theResource && gbResource.type == resourceType) {
+				theResource = gbResource.resource;
+			}
+		});
+	}
+	return theResource;
+}
+
+
+/*
+	Returns an array of all of the genome builds that could be determined
+	from bam and vcf headers.  In the correct case, only one (or zero)
+	row will be present in the returned array, representing the build that
+	is specified in all of the files:
+		[{species: the_species_object, build: the_build_object, from: [all bam and vcf files specified as proband,bam]}]
+	In cases where different builds have been specified, more than one row is 
+	returned: 
+		[{species: (Human), build: (build GRCh37), from: [{type:vcf, relationship: mother}, {type:bam, relationship: mother}]}]
+		[{species: (Human), build: (build GRCh38), from: [{type:vcf, relationship: proband},{type:bam, relationship: proband}]}]
+*/
+GenomeBuildHelper.prototype.getBuildsInHeaders = function(bamHeaderMap, vcfHeaderMap) {
+	var me = this;
+	var theBuilds = [];
+
+	for (relationship in bamHeaderMap) {
+		var header = bamHeaderMap[relationship];
+		var buildInfo = me.getBuildFromBamHeader(header);
+		me.parseBuildInfo(buildInfo, relationship, 'bam', theBuilds);
+	}			
+	for (relationship in vcfHeaderMap) {
+		var header = vcfHeaderMap[relationship];
+		var buildInfo = me.getBuildFromVcfHeader(header);
+		me.parseBuildInfo(buildInfo, relationship, 'vcf', theBuilds);
+	}			
+
+	return theBuilds;
+}
+
+
+GenomeBuildHelper.prototype.getBuildFromBamHeader = function(header) {
+	var me = this;
+	var buildInfo = {species: null, build: null, references: {}};
+
+	if (header) {
+		var lines = header.split("\n");
+		for ( var i=0; i<lines.length > 0; i++) {
+			 var fields = lines[i].split("\t");
+			 if (fields[0] == "@SQ") {
+			    var fieldMap = {};
+			    fields.forEach(function(field) {
+			      var values = field.split(':');
+			      fieldMap[ values[0] ] = values[1]
+			    })
+			    var refName   = fieldMap["SN"];
+			    var refLength = 1+parseInt(fieldMap["LN"]);
+			    var species   = fieldMap["SP"];
+			    var assembly  = fieldMap["AS"]
+			    if (refName && refLength) {
+					buildInfo.references[refName] = refLength;
+			    }
+			    if (!buildInfo.species && species ) {
+				    buildInfo.species = species;
+			    }
+			    if (!buildInfo.build && assembly) {
+			    	buildInfo.build = assembly;
+			    }
+			 }
+		}	
+	}
+
+	return buildInfo;
+}
+
+GenomeBuildHelper.prototype.getBuildFromVcfHeader = function(header) {
+    var me = this;
+
+	var buildInfo = {species: null, build: null, references: {}};
+	if (header) {
+		header.split("\n").forEach(function(headerRec) {
+		  if (headerRec.indexOf("##contig=<") == 0) {
+		    var allFields = headerRec.split("##contig=<")[1];
+
+		    var fields = allFields.split(/[,>]/);
+		    var refName = null;
+		    var refLength = null;
+		    fields.forEach(function(field) {
+		      if (field.indexOf("ID=") == 0) {
+		        refName = field.split("ID=")[1];
+		      }
+		      if (field.indexOf("length=") == 0) {
+		        refLength = field.split("length=")[1];
+		      }
+		      if (!buildInfo.build && field.indexOf("assembly=") == 0) {
+		        var buildString = field.split("assembly=")[1];
+		        if (buildString.indexOf("\"") == 0) {
+		          buildInfo.build = buildString.split("\"")[1];
+		        } else if (buildString.indexOf("'") == 0) {
+		          buildInfo.build = buildString.split("'")[1];
+		        } else {
+		          buildInfo.build = buildString;
+		        }
+		      }
+		      if (!buildInfo.species && field.indexOf("species=") == 0) {
+		        var speciesString = field.split("species=")[1];
+		        if (speciesString.indexOf("\"") == 0) {
+		          buildInfo.species = speciesString.split("\"")[1];
+		        } else if (speciesString.indexOf("'") == 0) {
+		          buildInfo.species = speciesString.split("'")[1];
+		        } else {
+		          buildInfo.species = speciesString;
+		        }
+		      }
+		    })
+		    if (refName && refLength) {
+		      buildInfo.references[refName] = refLength;
+		    }
+		  }
+		});
+	}
+	return buildInfo;
+}
+
+
+GenomeBuildHelper.prototype.parseBuildInfo = function(buildInfo, relationship, type, theBuilds) {
+	var me = this;
+
+	if (buildInfo == null || (buildInfo.species == null && buildInfo.build == null && (buildInfo.references == null || Object.keys(buildInfo.references).length == 0))) {
+		// We don't have any information in the file to find the species and build
+	} else {
+		// We have build info from the file.  Now try to match it to a known species and build
+		var speciesBuild = me.getProperSpeciesAndBuild(buildInfo);
+		if (speciesBuild.species && speciesBuild.build) {
+			if (theBuilds.length == 0) {
+				// TODO:  Need to indicate which data files (proband-bam, mother-bam, father-vcf, etc)
+				// that have this build
+				theBuilds.push( {species: speciesBuild.species, build: speciesBuild.build, from: [{type: type, relationship: relationship}]});
+			} else {
+				var foundAggregate = null;
+				theBuilds.forEach(function(aggregateSpeciesBuild) {
+					if (aggregateSpeciesBuild.species == speciesBuild.species && aggregateSpeciesBuild.build == speciesBuild.build) {
+						foundAggregate = aggregateSpeciesBuild;
+					}
+				});
+				if (foundAggregate) {
+					from = foundAggregate.from;
+					from.push({type: type, relationship: relationship});
+
+				} else {
+					theBuilds.push( {species: speciesBuild.species, build: speciesBuild.build, from: [{type: type, relationship: relationship}]});
+				}
+
+			}				
+		}
+
+	}
+
+}
+
+
+/*
+	Given the species and build names in the file header, try to find the corresponding
+	species and genome build based on matching the header names to the names (name, binomialName, latin_name)
+	of the species and the names (name and aliases) of genome build. 
+*/
+GenomeBuildHelper.prototype.getProperSpeciesAndBuild = function(buildInfo) {
+	var me = this;
+	var matchedSpecies = null;
+	var matchedBuild = null;
+
+	if (buildInfo != null) {
+		// If the bam header provided a species, make sure it matches the
+		// selected species name (or latin or binomial name).
+		if (buildInfo.species) {
+			for (speciesName in me.speciesNameToSpecies) {
+				if (!matchedSpecies) {
+					var species = me.speciesNameToSpecies[speciesName];
+					if (species.name == buildInfo.species || species.binomialName == buildInfo.species || species.latin_name ==  buildInfo.species ) {
+						matchedSpecies = species;
+					} 					
+				}
+			} 
+		}
+
+		// For now, just default to Human if species can't be determined from file headers
+		if (!matchedSpecies) {
+			matchedSpecies = me.speciesNameToSpecies[me.DEFAULT_SPECIES];
+		}
+
+		// Make sure each bam has a build that matches the selected
+		// build name or one of its aliases
+		if (matchedSpecies) {
+			if (buildInfo.build) {
+				matchedSpecies.genomeBuilds.forEach(function(build) {
+					if (!matchedBuild) {
+						if (build.name == buildInfo.build) {
+							matchedBuild = build;
+						} else {
+							build.aliases.forEach(function(gbAlias) {
+								if (gbAlias.alias == buildInfo.build) {
+									matchedBuild = build;
+								}
+							})
+						}																
+					}
+				})
+							
+			} else {
+				// If a build wasn't specified, try to match to a genome build based on reference lengths
+				matchedSpecies.genomeBuilds.forEach(function(build) {
+					if (!matchedBuild) {
+						var matchedCount = 0;
+						var notMatchedCount = 0;
+						var notFoundCount = 0;
+						build.references.forEach(function(reference) {
+							var refLength = null;
+							if (buildInfo.references[reference.name]) {
+								refLength = buildInfo.references[reference.name];
+							} else if (buildInfo.references[reference.alias]) {
+								refLength = buildInfo.references[reference.alias];
+							}
+							if (refLength && refLength == reference.length) {
+								matchedCount++;
+							} else if (refLength && refLength == reference.length - 1) {
+								matchedCount++;
+							} else if (refLength && refLength == reference.length + 1) {
+								matchedCount++;
+							} else if (refLength && refLength != reference.length) {
+								notMatchedCount++;
+							} else {
+								notFoundCount++;
+							}
+						});
+						if (build.references.length == matchedCount) {
+							matchedBuild = build;
+						} else if (matchedCount > 0 && notMatchedCount == 0 && (matchedCount + notFoundCount == build.references.length)) {
+							matchedBuild = build;
+						}
+
+					}
+
+				})
+
+			}
+		}
+	}
+	return {species: matchedSpecies, build: matchedBuild};
+
+
+}
+
+GenomeBuildHelper.prototype.formatIncompatibleBuildsMessage = function(buildsInData) {
+	var message = null;	
+	if (buildsInData && buildsInData.length > 1) {
+		message = "Incompatible builds in files.";
+		buildsInData.forEach(function(buildInfo) {
+			message += "<br>Build " + buildInfo.species.name + " " + buildInfo.build.name + " specified in ";
+			var fromCount = 0;
+			buildInfo.from.forEach(function(fileInfo) {
+				if (fromCount > 0) {
+					message += ", ";
+				}
+				message += fileInfo.relationship + " " + fileInfo.type;
+				fromCount++;
+			});
+			message += ".";
+		});
+
+	}
+	return message;	
+}
+
+

app/vcf.iobio.js
@@ -67,35 +67,6 @@ vcfiobio = function module() {
 
   var samples = [];
 
-  var refLengths_GRCh37 = 
-  {
-        "1":   +249250621,
-        "2":   +243199373,
-        "3":   +198022430,
-        "4":   +191154276,
-        "5":   +180915260,
-        "6":   +171115067,
-        "7":   +159138663,
-        "8":   +146364022,
-        "9":   +141213431,
-        "10":  +135534747,
-        "11":  +135006516,
-        "12":  +133851895,
-        "13":  +115169878,
-        "14":  +107349540,
-        "15":  +102531392,
-        "16":  +90354753,
-        "17":  +81195210,
-        "18":  +78077248,
-        "19":  +59128983,
-        "20":  +63025520,
-        "21":  +48129895,
-        "22":  +51304566,
-        "X":   +155270560,
-        "Y":   +59373566
-      };
-
-
 
   var emailServer            = "ws://nv-dev.iobio.io/email/";
 
@@ -339,7 +310,7 @@ vcfiobio = function module() {
         var indexseq = tbiIdx.idxContent.indexseq[i];
         var calcRefLength = indexseq.n_intv * size16kb;
 
-        var refLength = refLengths_GRCh37[me.stripChr(ref)];
+        var refLength = genomeBuildHelper.getReferenceLength(ref);
 
         // Use the linear index to load the estimated density data
         var intervalPoints = [];
@@ -393,7 +364,7 @@ vcfiobio = function module() {
 
           // Make sure to zero fill to the end of the reference
           var calcRefLength = pointData[pointData.length - 1].pos + size16kb;
-          var refLength = refLengths_GRCh37[me.stripChr(refName)];
+          var refLength = genomeBuildHelper.getReferenceLength(refName);
           if (refLength == null) {
             refLength = calcRefLength;
           }
@@ -528,7 +499,7 @@ vcfiobio = function module() {
               refName = tokens[1];     
 
               var calcRefLength = tokens[2];
-              var refLength = refLengths_GRCh37[me.stripChr(refName)];
+              var refLength = genomeBuildHelper.getReferenceLength(refName);
               if (refLength == null) {
                  refLength = calcRefLength;
               }
@@ -809,6 +780,48 @@ vcfiobio = function module() {
     return allPoints;
   }
 
+  exports.getHeader = function(callback) {
+    var me = this;
+    var headerStr = "";
+    if (sourceType.toLowerCase() == SOURCE_TYPE_URL.toLowerCase() && me.vcfURL != null) {
+
+        var buffer = "";
+        var cmd = new iobio.cmd(
+            tabix,
+            ['-H', me.vcfURL]
+        );
+
+        cmd.on('data', function(data) {
+          if (data != undefined) {
+            success = true;
+            buffer += data;
+          }
+        });
+
+        cmd.on('end', function() {
+          if (success == null) {
+            success = true;
+          }
+          if (success && buffer.length > 0) {
+            headerStr = buffer;
+            callback(headerStr);
+          }
+        });
+        cmd.run();
+        
+      } else if (vcfFile) {
+          var vcfReader = new readBinaryVCF(tabixFile, vcfFile, function(tbiR) {
+            vcfReader.getHeader( function(theHeader) {              
+              callback(theHeader);
+            });
+          });
+      } else {
+        callback(null);
+      }
+
+  }
+
+
 
   exports.getStats = function(refs, options, callback) {    
     if (sourceType == SOURCE_TYPE_URL) {
@@ -1026,8 +1039,8 @@ vcfiobio = function module() {
       sampleNames.length = 0;
 
       var headerRecords = [];
-      vcfReader.getHeader( function(header) {
-         headerRecords = header.split("\n");
+      vcfReader.getHeader( function(headerStr) {
+         headerRecords = headerStr.split("\n");
          headerRecords.forEach(function(headerRec) {
             if (headerRec.indexOf("#CHROM") == 0) {
               var headerFields = headerRec.split("\t");

assets/css/vcf.iobio.css
@@ -1,9 +1,40 @@
 label {
     font-weight: 300;
 }
-#vcf-url-go-button  { 
+.go-button  { 
   font:300 28px quicksand;
   vertical-align:middle;
+  margin-top: 20px;
+  float: right;
+}
+
+#go-panel {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    text-align: center;
+    margin-left: auto;
+    width: 670px;
+    margin-right: auto;
+    overflow: visible;
+    clear: both;
+}
+
+#file-alert {
+    visibility:hidden;
+    min-height:50px;
+    text-align:center;
+    margin-top:10px;
+    margin-left:auto; 
+    margin-right:auto;
+    width:700px;
+    font-weight: 400;
+    font-size:16px;
+}
+
+#build-label {
+    font-size: 26px;
+    color: #2687BE;    
+    margin-right: 20px;
 }
 
 

commit 6fe216269b02dc84c576f386f0b1f7ff58457c53

Fix bug introduced with commit 5f135d258dc68ccf20282b4fdbcd82eb58c41a13 which deleted lines of code in init() function that was initializing sample dropdown.

app/app.js
@@ -124,6 +124,15 @@ function init() {
 	    dataSelect.setDefaultBuildFromData();
   	});
 
+	$('#vcf-sample-select').selectize(
+		{
+			create: true,
+			maxItems: null,
+			valueField: 'value',
+	    	labelField: 'value',
+	    	searchField: ['value']
+		}
+	);
 
 	// Setup event handlers for File input
 	document.getElementById('file').addEventListener('change', onFilesSelected, false);